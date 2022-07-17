07c28f361ed9d6cb40dabefd1450f9d1d6cc1725
Sergey <gusevsergey85@gmail.com>
2022-07-17 17:58:18 +0300

    Created function

--- function in C# Seminar2/ThirdDigit/Program.cs
+++ function in C# Seminar2/ThirdDigit/Program.cs
@@ -4,7 +4,7 @@
 78 -> третьей цифры нет
 32679 -> 6
 */
-Console.WriteLine("Введите число ");
+/* Console.WriteLine("Введите число ");
 string number = Console.ReadLine();
 string startNumber = number;
 if (number.Length < 3)
@@ -23,3 +23,32 @@ else
     Console.WriteLine("Третья цифра числа " + startNumber + " " + "равна " + thirdDigit);
 
 }
+*/
+// с помощью функции
+
+string GetThirdNumber(int number)
+{
+    string number1 = number + "";
+    string startNumber = number1;
+    if (number1.Length < 3)
+    {
+        return "Третьей цифры нет";
+    }
+    else
+    {
+        while (number1.Length > 3)
+        {
+            number1 = (int.Parse(number1) / 10) + "";
+
+
+        }
+        
+        return int.Parse(number1) % 10 + ""; 
+
+    }
+    
+}
+
+Console.WriteLine(GetThirdNumber(645));
+Console.WriteLine(GetThirdNumber(72));
+Console.WriteLine(GetThirdNumber(32679));
