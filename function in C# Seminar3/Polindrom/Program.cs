dce51a176f3040f6afd85b26205eab3d98604dce
Sergey <gusevsergey85@gmail.com>
2022-07-18 15:13:59 +0300

    Polindrom

---
+++ function in C# Seminar3/Polindrom/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите 5-е число");
+string number = Console.ReadLine();
+isPolindrom(number);
+
+void isPolindrom(string number)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
