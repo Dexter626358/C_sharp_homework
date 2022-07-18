9d900390c16cc7095ac785fb07e3b6847150dae9
Sergey <gusevsergey85@gmail.com>
2022-07-18 16:16:20 +0300

    Cub numbers

---
+++ function in C# Seminar3/CubNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+CubNumbers(number);
+
+
+void CubNumbers(int num)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.Write(i * i * i + " ");
+    }
+}
