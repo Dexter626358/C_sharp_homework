a034dd1f572b805701cccc131ccbef67699e6936
Sergey <gusevsergey85@gmail.com>
2022-08-03 18:59:00 +0300

    task3

--- function in C# Seminar4/Task3/Program.cs
+++ function in C# Seminar4/Task3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 29: Напишите программу, которая задаёт массив
+ из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33] */
+
+Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine());
+int[] array = new int[size];
+Random rnd = new Random();
+for (int i = 0; i < size; i++)
+{
+    array[i] = rnd.Next(-100, 100);
+}
+
+Console.WriteLine();
+Console.Write("[");
+string newStr = String.Join(", ", array);
+Console.WriteLine(newStr + "]");
+
+
+
+
