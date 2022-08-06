f7f2e213690fc45baa1dc464bdeb67251b213640
Sergey <gusevsergey85@gmail.com>
2022-08-06 12:26:09 +0300

    Task2

---
+++ function in C# Seminar5/Task36/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 36: Задайте одномерный массив,
+ заполненный случайными числами.
+ Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+int elements = 10;
+int result = sumOddIndexes(fillArray(elements));
+Console.WriteLine("Сумма элементов, стоящих на нечётных позициях равна: " + result);
+
+int[] fillArray(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(-1000, 1000);
+    }
+    return array;
+}
+
+int sumOddIndexes(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+
