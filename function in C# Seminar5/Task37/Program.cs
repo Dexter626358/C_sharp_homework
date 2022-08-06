600b566ed33401e9c63dc25ff37c88094c8bba58
Sergey <gusevsergey85@gmail.com>
2022-08-06 13:56:34 +0300

    Added functions

--- function in C# Seminar5/Task37/Program.cs
+++ function in C# Seminar5/Task37/Program.cs
@@ -3,29 +3,37 @@
  Результат запишите в новом массиве.
 [1 2 3 4 5] -> 5 8 3
 [6 7 3 6] -> 36 21 */
-int[] array = {6, 7, 3, 6};
-int len = array.Length;
-int[] result = new int[len / 2 + len % 2];
-int index = 0;
-if (len % 2 == 0)
+int[] array = {1, 2, 3, 4, 5, 6, 7};
+int[] result = new int[array.Length / 2 + 1];
+
+if (array.Length % 2 == 0)
 {
-    while(index < (len / 2))
-{
-    result[index] = array[index] * array[len - index - 1];
-    index++;
-}
+    printArray(multArray(array));
 } else
 {
+    result = multArray(array);
+    result[result.Length - 1] = array[array.Length / 2];
+    printArray(result);
+}
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+
+}
+    
+int[] multArray(int[] array)
+{
+    int len = array.Length;
+    int[] newArray = new int[len / 2 + len % 2];
+    int index = 0;
     while(index < (len / 2))
-{
-    result[index] = array[index] * array[len - index - 1];
-    index++;
+    {
+        newArray[index] = array[index] * array[len - index - 1];
+        index++;
+    }
+    return newArray; 
 }
-result[result.Length - 1] = array[len / 2];
-}
-
-for (int i = 0; i < result.Length; i++)
-{
-    Console.Write(result[i] + " ");
-}
-
