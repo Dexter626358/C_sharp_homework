e1bb12c79fcad5e28a2af33dd1807c9c57b9dd36
Sergey <gusevsergey85@gmail.com>
2022-08-06 11:57:58 +0300

    The end

---
+++ function in C# Seminar5/Task34/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными 
+трёхзначными числами. Напишите программу, которая покажет количество 
+чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+int elements = 100;
+
+int result = evenNumbers(fillArray(elements));
+Console.WriteLine("Количество четных чисел равно: " + result);
+
+
+int[] fillArray(int size)
+{
+    int[] positivArray = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        positivArray[i] = rnd.Next(100, 1000);
+    }
+    // String sentence = string.Join(", ", positivArray);
+    // Console.WriteLine(sentence);
+    return positivArray;
+}
+int evenNumbers(int[] array)
+{
+    int countEven = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) countEven++;
+    }
+    return countEven;
+}
+
+
