3ccdc1f43181391964b8930d4d6530f18139dd7f
Sergey <gusevsergey85@gmail.com>
2022-08-06 13:35:00 +0300

    solving without function

---
+++ function in C# Seminar5/Task37/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 37: Найдите произведение пар чисел в одномерном массиве.
+ Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+ Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21 */
+int[] array = {6, 7, 3, 6};
+int len = array.Length;
+int[] result = new int[len / 2 + len % 2];
+int index = 0;
+if (len % 2 == 0)
+{
+    while(index < (len / 2))
+{
+    result[index] = array[index] * array[len - index - 1];
+    index++;
+}
+} else
+{
+    while(index < (len / 2))
+{
+    result[index] = array[index] * array[len - index - 1];
+    index++;
+}
+result[result.Length - 1] = array[len / 2];
+}
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.Write(result[i] + " ");
+}
+
