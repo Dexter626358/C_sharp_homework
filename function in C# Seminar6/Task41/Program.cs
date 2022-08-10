a3801cb50e65bd856a67ed9b153e3ea20224d1d0
Sergey <gusevsergey85@gmail.com>
2022-08-10 12:58:23 +0300

    task41

---
+++ function in C# Seminar6/Task41/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел.
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+Console.WriteLine("Введите количество чисел: ");
+int m = int.Parse(Console.ReadLine());
+int countPositive = 0;
+int readNumber;
+for (int i = 1; i <= m; i++)
+{
+    Console.WriteLine("Введите число " + i);
+    readNumber = int.Parse(Console.ReadLine());
+    if (readNumber > 0) countPositive++;
+}
+Console.WriteLine("Количество положительных чисел равно " + countPositive);
