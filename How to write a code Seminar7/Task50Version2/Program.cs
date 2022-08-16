3f19da08aa5d045f00a5d1f81800af2ad7fd02d1
Sergey <gusevsergey85@gmail.com>
2022-08-16 21:20:56 +0300

    Second

---
+++ How to write a code Seminar7/Task50Version2/Program.cs
@@ -0,0 +1,27 @@
+﻿int [,] array = {{1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}};
+string t = "";
+Console.WriteLine("Введите координаты точки (через пробел):");
+t = Console.ReadLine();
+string[] tv = t.Split(' ').Where(x=>x !="").ToArray();
+int row = int.Parse(tv[0]);
+int col = int.Parse(tv[1]);
+bool flag = false;
+
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        if (i == row && j == col)
+        {
+            flag = true;
+            break;
+        }
+    }
+}
+if (flag)
+{
+    Console.WriteLine("Значение элемента равно " + array[row, col]);
+} else
+{
+    Console.WriteLine("Такого элемента нет");
+}
